7920fc80eeaba32b12243dbe4aed0cf091046151
Rodo <rodolfo.raskovsky85@gmail.com>
2018-04-06 11:48:35 -0300

    puerto

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ app.get('/*', function (req, res) {
 		res.sendFile(__dirname + '/index.html');
 	}
 });
-app.listen(8080, function () {
-  console.log('Microservice listening on port 8080!');
+app.listen(80, function () {
+  console.log('Microservice listening on port 80!');
 });
 
 
